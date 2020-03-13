0fc043ae3f30a09a3f42343c938eae227959110c
jamesjsewell <jamesjosephsewell@gmail.com>
2020-03-13 15:56:48 -0500

    renders todos, adds edit todo base functionality

--- backend/sql/todos.sql
+++ backend/sql/todos.sql
@@ -1,12 +1,13 @@
-CREATE TABLE categories(
-    id SERIAL NOT NULL PRIMARY KEY,
-    title varchar(20),
-    body varchar(40)
-);
-
+-- CREATE TABLE categories(
+--     id SERIAL NOT NULL PRIMARY KEY,
+--     title varchar(20),
+--     body varchar(40)
+-- );
+DROP TABLE todos;
 CREATE TABLE todos(
     id SERIAL NOT NULL PRIMARY KEY,
     title varchar(20),
-    body varchar(40),
-    category integer REFERENCES categories(id)
+    body varchar(250),
+    category integer REFERENCES categories(id),
+    done BOOLEAN
 );
--- index.js
+++ index.js
@@ -11,10 +11,6 @@ function addTodo(e) {
   });
 }
 
-function updateTodo() {}
-
-function deleteTodo() {}
-
 document.getElementById('todo-form').addEventListener('submit', addTodo);
 
 // routing
@@ -23,8 +19,101 @@ function renderCategoriesPage() {
   document.getElementById('todos-container').className = 'hide';
 }
 
+function renderTodos() {
+  fetch('http://localhost:3000/api/todos')
+    .then(res => res.json())
+    .then(todos => {
+      if (!todos.length) {
+        document.getElementById('todos-in-progress-container').innerHTML = '';
+        document.getElementById('todos-done-container').innerHTML = '';
+        return;
+      }
+      for (let i = 0; i < todos.length; i++) {
+        const todo = todos[i];
+        document.getElementById(
+          !todo.done ? 'todos-in-progress-container' : 'todos-done-container'
+        ).innerHTML += `
+                <div id="todo-${i}" class="todo">
+                    <div id="todo-contents-${i}" class="todo-contents">
+                        <div class="todo-title-wrapper row">
+                            <div class="nine columns">
+                                <h5>${todo.title}</h5>
+                            </div>
+                            <div class="three columns">
+                                <button id="edit-todo-button-${i}" todo="${i}" class="todo-edit-button u-pull-right"><i class="material-icons">create</i></button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="twelve columns">
+                                <p>${todo.body}</h5>
+                            </div>
+                        
+                        </div>
+                        <div class="row">
+                            <div class="twelve columns">
+                                <label for="todo-complete-toggle">done</label>
+                                <input id="todo-complete-toggle" type="checkbox" />
+                            </div>
+                        </div>
+                    </div>
+                    <form id="edit-todo-form-${i}" class="edit-todo-form hide">
+                        <input type="text" placeholder="title" value="${todo.title}"/>
+                        <select placeholder="category">
+                            <option value="" disabled selected>select category</option>
+                        </select>
+                        <textarea placeholder="description">${todo.body}</textarea>
+                        <div class="edit-todo-actions-wrapper">
+                            <button class="cancel-edit-todo" id="cancel-edit-${i}" type="submit">cancel</button>
+                            <button class="save-edit-todo button-primary" id="save-edit-${i}" type="submit">save</button>
+                        </div>
+                    </form>
+                </div>
+            `;
+      }
+
+      function editTodo(e) {
+        function showEditForm(i) {
+          document.getElementById(`todo-contents-${i}`).classList.add('hide');
+          document
+            .getElementById(`edit-todo-form-${i}`)
+            .classList.remove('hide');
+        }
+
+        function hideEditForm(i) {
+          document
+            .getElementById(`todo-contents-${i}`)
+            .classList.remove('hide');
+          document.getElementById(`edit-todo-form-${i}`).classList.add('hide');
+        }
+        const todoId = e.currentTarget.getAttributeNode('todo').value;
+        showEditForm(todoId);
+
+        const cancelEditButton = document.getElementById(
+          `cancel-edit-${todoId}`
+        );
+        const saveEditButton = document.getElementById(`save-edit-${todoId}`);
+
+        cancelEditButton.addEventListener('click', e => {
+          document
+            .getElementById(`edit-todo-button-${todoId}`)
+            .addEventListener('click', editTodo);
+          hideEditForm(todoId);
+        });
+
+        saveEditButton.addEventListener('click', e => {
+          console.log(e);
+        });
+      }
+      const editButtons = document.querySelectorAll('.todo-edit-button');
+      for (let i = 0; i < editButtons.length; i++) {
+        editButtons[i].addEventListener('click', editTodo);
+      }
+    });
+}
+
 function renderTodosPage() {
   document.getElementById('todos-container').className = '';
+  renderTodos();
 }
 
 function router() {
